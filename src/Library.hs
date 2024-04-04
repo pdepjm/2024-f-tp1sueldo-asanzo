1cfa3566775710de9696ba285fff430267fc753d
Alfredo Sanzo <alfredo.sanzo@gmail.com>
2024-04-04 19:49:40 -0300

    solución

--- src/Library.hs
+++ src/Library.hs
@@ -3,4 +3,16 @@
 module Library where
 import PdePreludat
 
-sueldo = implementame
+sueldo cargo antig horas = base cargo * porcAntig antig * proporcionalidad horas
+
+base "titular" = 149000
+base "adjunto" = 116000
+base "ayudante" = 66000
+
+porcAntig antig | antig < 3 = 1
+    | antig < 5 = 1.2
+    | antig < 10 = 1.3
+    | antig < 24 = 1.5
+    | otherwise = 2.2
+
+proporcionalidad horas = round (horas / 10)
